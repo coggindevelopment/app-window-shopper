df3835057916557564d6e0915a3aa2df893d466a
Nicholas Coggin <nicholascoggin@Nicholass-MacBook-Pro.local>
2019-01-19 11:59:17 -0600

    Custom Drawing in iOS 12 with drawRect

--- window-shopper/Model/Wage.swift
+++ window-shopper/Model/Wage.swift
@@ -10,6 +10,6 @@ import Foundation
 
 class Wage {
     class func getHours(forWage wage: Double, andPrice price: Double) -> Int {
-        return Int(round(price / wage))
+        return Int(ceil(price / wage))
     }
 }
--- window-shopper/Views/CurrencyTxtField.swift
+++ window-shopper/Views/CurrencyTxtField.swift
@@ -11,6 +11,22 @@ import UIKit
 @IBDesignable
 class CurrencyTxtField: UITextField {
     
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLbl = UILabel(frame: CGRect(x: 10, y: (frame.size.height / 2) - size / 2, width: size, height: size))
+        currencyLbl.backgroundColor = #colorLiteral(red: 0.765707612, green: 0.7699981332, blue: 0.7805780768, alpha: 0.8024867958)
+        currencyLbl.textAlignment = .center
+        currencyLbl.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        currencyLbl.layer.cornerRadius = 5.0
+        currencyLbl.clipsToBounds = true
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLbl.text = formatter.currencySymbol
+        addSubview(currencyLbl)
+    }
+    
     override func prepareForInterfaceBuilder() {
         customizeView()
     }
@@ -24,6 +40,7 @@ class CurrencyTxtField: UITextField {
         backgroundColor = #colorLiteral(red: 0.9953911901, green: 0.9881951213, blue: 1, alpha: 0.2488721391)
         layer.cornerRadius = 5.0
         textAlignment = .center
+        clipsToBounds = true
         
         if let p = placeholder {
             let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)])
