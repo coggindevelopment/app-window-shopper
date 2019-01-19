aee3f47ceda42d1c14712573dab3774591cb752b
Nicholas Coggin <nicholascoggin@Nicholass-MacBook-Pro.local>
2019-01-19 11:19:56 -0600

    lesson-03 commit - Creating Simple Unit Tests

---
+++ window-shopper/Model/Wage.swift
@@ -0,0 +1,15 @@
+//
+//  Wage.swift
+//  window-shopper
+//
+//  Created by Nicholas Coggin on 1/19/19.
+//  Copyright © 2019 Nicholas Coggin. All rights reserved.
+//
+
+import Foundation
+
+class Wage {
+    class func getHours(forWage wage: Double, andPrice price: Double) -> Int {
+        return Int(round(price / wage))
+    }
+}
